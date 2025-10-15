40e8203579879fa37d10ffad6026e8b49a6207b9
Anoop Negi <anoopnegi123a@gmail.com>
2025-10-15 13:38:03 +0000

    added postgres compatible query string

--- Program.cs
+++ Program.cs
@@ -81,14 +81,15 @@ app.MapPost("/board",async (HttpRequest request) => {
    using var connection = new NpgsqlConnection(connectionString);
    connection.Open();  
    var command = connection.CreateCommand(); 
-   command.CommandText = @"insert into board values($id, $port_number, $power_socket, $port_status, $other, $ip_address)";
-   
-   command.Parameters.AddWithValue("$id", id);
-   command.Parameters.AddWithValue("$port_number", port_number);
-   command.Parameters.AddWithValue("$power_socket", power_socket);
-   command.Parameters.AddWithValue("$port_status", port_status); 
-   command.Parameters.AddWithValue("$other",other);  
-   command.Parameters.AddWithValue("$ip_address",ip_address); 
+   command.CommandText = @"insert into board (id, port_number, power_socket, port_status, other, ip_address) 
+                            values($1, $2, $3, $4, $5, $6)";
+    
+    command.Parameters.AddWithValue("$1", id);
+    command.Parameters.AddWithValue("$2", port_number);
+    command.Parameters.AddWithValue("$3", power_socket);
+    command.Parameters.AddWithValue("$4", port_status); 
+    command.Parameters.AddWithValue("$5", other);  
+    command.Parameters.AddWithValue("$6", ip_address); 
    command.ExecuteNonQuery();
    return Results.Ok("got in database");   
 }); 
