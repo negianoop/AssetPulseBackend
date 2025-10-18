53f74f34529abade745fc4f9b37d5eaa4a18265e
Anoop Negi <anoopnegi123a@gmail.com>
2025-10-18 11:29:20 +0000

    hello

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@ app.MapGet("/board", () =>
                 row["id"] = reader.GetInt32(0);
                 row["port_number"] = reader.GetInt32(1);
                 row["ip_address"] = reader.GetString(2);
-                row["port_status"] = reader.GetBoolean(3);
+                row["port_status"] = reader.GetString(3).ToLower() == "t";
                 row["other"] = reader.GetString(4);
                 row["power_socket"] = reader.GetInt32(5);
 
